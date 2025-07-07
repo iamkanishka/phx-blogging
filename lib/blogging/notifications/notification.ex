325a2a3889de9faf4b59c89455513432262da193
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-07 21:20:14 +0530

    feat: notification  schema

---
+++ lib/blogging/notifications/notification.ex
@@ -0,0 +1,22 @@
+defmodule Blogging.Notifications.Notification do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @types [:comment, :reaction, :mention]
+
+  schema "notifications" do
+    field :type, Ecto.Enum, values: @types
+    field :data, :map
+    field :read_at, :naive_datetime
+
+    belongs_to :user, Blogging.Accounts.User
+
+    timestamps()
+  end
+
+  def changeset(notification, attrs) do
+    notification
+    |> cast(attrs, [:type, :data, :user_id, :read_at])
+    |> validate_required([:type, :data, :user_id])
+  end
+end
