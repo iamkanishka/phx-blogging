630c9ec15de8dca8494a0446876fa3cfa84fc8ec
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-09 20:49:58 +0530

    Added binary_type for id

--- lib/blogging/repo.ex
+++ lib/blogging/repo.ex
@@ -2,4 +2,7 @@ defmodule Blogging.Repo do
   use Ecto.Repo,
     otp_app: :blogging,
     adapter: Ecto.Adapters.Postgres
+
+  @migration_primary_key [type: :binary_id]
+  @migration_foreign_key [type: :binary_id]
 end
