3be02ae1f5c78b860d1bf8cc10a5eb3be3e6d72a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-09 21:11:58 +0530

    CreateComments migration

---
+++ priv/repo/migrations/20250707115618_create_comments.exs
@@ -0,0 +1,29 @@
+defmodule Blogging.Repo.Migrations.CreateComments do
+  use Ecto.Migration
+
+  def change do
+    # Enable ltree extension if path field is used
+
+    create table(:comments, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :content, :text, null: false
+      add :depth, :integer, default: 0, null: false
+
+      add :user_id, references(:users, on_delete: :delete_all, type: :binary_id), null: false
+      add :post_id, references(:posts, on_delete: :delete_all, type: :binary_id), null: false
+      add :parent_id, references(:comments, on_delete: :delete_all, type: :binary_id)
+
+      timestamps()
+    end
+
+    create index(:comments, [:user_id])
+    create index(:comments, [:post_id])
+    create index(:comments, [:parent_id])
+    # For efficient ltree queries
+    create index(:comments, [:path], using: :gist)
+  end
+
+  def down do
+    drop table(:comments)
+  end
+end
