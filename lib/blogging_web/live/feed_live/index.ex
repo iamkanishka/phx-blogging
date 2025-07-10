b4686dc29a9e3e8e8fd10fa48d94a75ab62bb38b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-10 21:28:44 +0530

    FeedLive.Index

---
+++ lib/blogging_web/live/feed_live/index.ex
@@ -0,0 +1,35 @@
+
+defmodule BloggingWeb.FeedLive.Index do
+  use BloggingWeb, :live_view
+
+  alias Blogging.Contents.Posts.{Posts}
+
+  @impl true
+  def mount(_params, _session, socket) do
+   IO.inspect(list_posts())
+    {:ok, assign(socket, :posts, list_posts())}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Posts")
+    |> assign(:post, nil)
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    post = Posts.get_post(id)
+    {:ok, _} = Posts.delete_post(post)
+
+    {:noreply, assign(socket, :posts, list_posts())}
+  end
+
+  defp list_posts do
+    Posts.list_posts()
+  end
+end
