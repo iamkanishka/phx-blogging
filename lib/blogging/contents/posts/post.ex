df995fb393e0df370225b29c15f316dabf23ab12
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-10 21:23:26 +0530

    chore: minor changes

--- lib/blogging/contents/posts/post.ex
+++ lib/blogging/contents/posts/post.ex
@@ -8,23 +8,26 @@ defmodule Blogging.Contents.Posts.Post do
 
   schema "posts" do
     field :title, :string
-    # Stores Quill's delta format as JSON
+    field :sub_title, :string
     field :html_content, :string
     field :tags, {:array, :string}
     field :view_count, :integer, default: 0
     field :is_published, :boolean, default: false
 
     belongs_to :user, Blogging.Accounts.User
-    has_many :comments, Blogging.Content.Comment
-    has_many :reactions, Blogging.Content.Reaction
+    has_many :comments, Blogging.Contents.Comments.Comment
+
+    has_many :reactions, Blogging.Contents.Reactions.Reaction,
+      foreign_key: :reactable_id,
+      where: [reactable_type: "post"]
 
     timestamps()
   end
 
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:title, :html_content, :tags, :user_id])
-    |> validate_required([:title, :html_content, :user_id])
+    |> cast(attrs, [:title, :sub_title, :html_content, :tags, :user_id])
+    |> validate_required([:title, :sub_title, :html_content, :user_id])
     |> validate_length(:title, max: 100)
     |> validate_length(:tags, max: 5)
   end
