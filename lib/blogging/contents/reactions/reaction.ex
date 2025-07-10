ad62c2b8024b734e2f84e50b69573d337af5f14e
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-10 21:23:55 +0530

    chore: minor addition and minor change

--- lib/blogging/contents/reactions/reaction.ex
+++ lib/blogging/contents/reactions/reaction.ex
@@ -2,16 +2,21 @@ defmodule Blogging.Contents.Reactions.Reaction do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @reaction_types ["like", "love", "wow", "laugh", "sad", "angry"]
+  alias Blogging.Repo
+  alias Blogging.Contents.Posts.Post
+  alias Blogging.Contents.Comments.Comment
+
+
+
+  @reaction_types ~w(like love wow laugh sad angry)
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @derive {Phoenix.Param, key: :id}
   @foreign_key_type :binary_id
   schema "reactions" do
     field :type, :string
-    # "post" or "comment"
     field :reactable_type, :string
-    field :reactable_id, :integer
+    field :reactable_id, :binary_id
 
     belongs_to :user, Blogging.Accounts.User
 
@@ -24,25 +29,23 @@ defmodule Blogging.Contents.Reactions.Reaction do
     |> validate_required([:type, :reactable_type, :reactable_id, :user_id])
     |> validate_inclusion(:type, @reaction_types)
     |> validate_inclusion(:reactable_type, ["post", "comment"])
-    |> unique_constraint([:user_id, :reactable_type, :reactable_id])
+    |> unique_constraint([:user_id, :reactable_type, :reactable_id],
+      name: :unique_user_reactable_reaction
+    )
     |> validate_reactable_exists()
   end
 
   defp validate_reactable_exists(changeset) do
     case {get_field(changeset, :reactable_type), get_field(changeset, :reactable_id)} do
       {"post", id} when is_integer(id) ->
-        if Blogging.Repo.get(Blogging.Content.Post, id) do
-          changeset
-        else
-          add_error(changeset, :reactable_id, "post does not exist")
-        end
+        if Repo.get(Post, id),
+          do: changeset,
+          else: add_error(changeset, :reactable_id, "post does not exist")
 
       {"comment", id} when is_integer(id) ->
-        if Blogging.Repo.get(Blogging.Content.Comment, id) do
-          changeset
-        else
-          add_error(changeset, :reactable_id, "comment does not exist")
-        end
+        if Repo.get(Comment, id),
+          do: changeset,
+          else: add_error(changeset, :reactable_id, "comment does not exist")
 
       _ ->
         changeset
