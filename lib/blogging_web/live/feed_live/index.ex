04a7a180733ace061dc867a4ce49ad242f32b3e8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-12 21:30:52 +0530

    Refactor BloggingWeb.FeedLive.Index to include user authentication and feed filtering

--- lib/blogging_web/live/feed_live/index.ex
+++ lib/blogging_web/live/feed_live/index.ex
@@ -1,35 +1,120 @@
-
 defmodule BloggingWeb.FeedLive.Index do
+  alias Blogging.Accounts
+  alias Blogging.Contents.Feeds.Feeds
   use BloggingWeb, :live_view
 
-  alias Blogging.Contents.Posts.{Posts}
+  @impl true
+  def mount(_params, session, socket) do
+    current_user = Accounts.get_user_by_session_token(session["user_token"])
+
+    {:ok,
+     socket
+     |> assign(:page_title, "Feed Blogging")
+     |> assign(:active_tab, "For you")
+
+     |> assign(:current_user, current_user)}
+  end
+
 
   @impl true
-  def mount(_params, _session, socket) do
-   IO.inspect(list_posts())
-    {:ok, assign(socket, :posts, list_posts())}
-  end
+def handle_params(params, _url, socket) do
+  current_user = socket.assigns.current_user
 
-  @impl true
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
+  cond do
+    params["feed"] == "following" ->
+      page = Feeds.list_network_posts(current_user, params)
+      {:noreply,
+       assign(socket,
+         active_tab: "Following",
+         posts: page.entries,
+         pagination: page
+       )}
 
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Listing Posts")
-    |> assign(:post, nil)
-  end
+    params["tag"] ->
+      tag = String.capitalize(params["tag"])
+      page = Feeds.list_by_tag(tag, current_user, params)
+      {:noreply,
+       assign(socket,
+         active_tab: tag,
+         posts: page.entries,
+         pagination: page
+       )}
 
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    post = Posts.get_post(id)
-    {:ok, _} = Posts.delete_post(post)
+    true ->
+      page =
+        Feeds.list_relevant_posts(current_user, %{
+          "page" => Map.get(params, "page", "1"),
+          "page_size" => Map.get(params, "page_size", "10")
+        })
 
-    {:noreply, assign(socket, :posts, list_posts())}
+      {:noreply,
+       assign(socket,
+         active_tab: "For you",
+         posts: page.entries,
+         pagination: page
+       )}
   end
+end
 
-  defp list_posts do
-    Posts.list_posts()
-  end
+
+
+  # @impl true
+  # def handle_params(params, _url, socket) do
+  #   #     page = Map.get(params, "page", "1") |> String.to_integer()
+  #   # page_size = Map.get(params, "page_size", "10") |> String.to_integer()
+  #   #  %{"page" => "1", "page_size" => "10"}
+
+  #   page =
+  #     Feeds.list_relevant_posts(socket.assigns.current_user, %{
+  #       "page" => "1",
+  #       "page_size" => "10"
+  #     })
+
+  #   page = Feeds.list_network_posts(socket.assigns.current_user, params)
+
+  #   {:noreply,
+  #    socket
+  #    |> assign(posts: page.entries)
+  #    |> assign(pagination: page)}
+
+  #   IO.inspect(page.entries)
+
+  #   {:noreply,
+  #    socket
+  #    |> assign(posts: page.entries)
+  #    |> assign(:pagination, page)}
+  # end
+
+
+#   @impl true
+# def handle_params(params, _url, socket) do
+#   current_user = socket.assigns.current_user
+#   feed = Map.get(params, "feed", "following")
+
+#   {page, active_tab} =
+#     case feed do
+#       "following" ->
+#         {
+#           Feeds.list_network_posts(current_user, params),
+#           "Following"
+#         }
+
+#       _ ->
+#         {
+#           Feeds.list_relevant_posts(current_user, %{
+#             "page" => Map.get(params, "page", "1"),
+#             "page_size" => Map.get(params, "page_size", "10")
+#           }),
+#           "For you"
+#         }
+#     end
+
+#   {:noreply,
+#    socket
+#    |> assign(:posts, page.entries)
+#    |> assign(:pagination, page)
+#    |> assign(:active_tab, active_tab)}
+# end
+
+
 end
