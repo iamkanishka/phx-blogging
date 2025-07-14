0fdfffa95285e3f4b6c7924872b96460d485a9d7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-14 21:13:17 +0530

    chor: Added fnctunality for editing

--- lib/blogging_web/live/post_live/create_edit.ex
+++ lib/blogging_web/live/post_live/create_edit.ex
@@ -1,23 +1,45 @@
 defmodule BloggingWeb.PostLive.CreateEdit do
+  use BloggingWeb, :live_view
+
   alias Blogging.Accounts
   alias Blogging.Contents.Posts.Post
   alias Blogging.Contents.Posts.Posts
 
-  use BloggingWeb, :live_view
-
   @impl true
   def mount(_params, session, socket) do
     user = Accounts.get_user_by_session_token(session["user_token"])
-    changeset = Posts.change_post(%Post{user_id: user.id})
 
-    {:ok,
+    {:ok, assign(socket, user: user)}
+  end
+
+  @impl true
+  def handle_params(%{"id" => id}, _url, socket) do
+    post = Posts.get_post(id)
+
+    {:noreply,
+     socket
+     |> assign(:page_title, "Edit Post")
+     |> assign(:post, post)
+     |> assign(:action, :edit)
+     |> assign(:user_id, post.user_id)
+     |> assign(:html_content, post.html_content || "")}
+  end
+
+  def handle_params(_params, _url, socket) do
+    post = %Post{user_id: socket.assigns.user.id}
+
+    {:noreply,
      socket
      |> assign(:page_title, "New Post")
+     |> assign(:post, post)
      |> assign(:action, :new)
-     |> assign(:user_id, user.id)
-     |> assign(:post, %Post{user_id: user.id})
-     |> assign(:changeset, changeset)
+     |> assign(:user_id, post.user_id)
      |> assign(:html_content, "")}
+  end
+
+  @impl true
+  def handle_info({BloggingWeb.PostLive.FormComponent, {:saved, post}}, socket) do
+    {:noreply, push_navigate(socket, to: "/posts")}
   end
 
   @impl true
