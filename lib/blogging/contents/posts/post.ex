9bc20ab5fe122260ce5394aac8c96f218cef36a8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-15 21:31:51 +0530

    Added wishlists attribute

--- lib/blogging/contents/posts/post.ex
+++ lib/blogging/contents/posts/post.ex
@@ -14,6 +14,9 @@ defmodule Blogging.Contents.Posts.Post do
     field :view_count, :integer, default: 0
     field :is_published, :boolean, default: false
 
+    has_many :wishlists, Blogging.Contents.Wishlists.Wishlist
+    has_many :wishlisted_by_users, through: [:wishlists, :user]
+
     belongs_to :user, Blogging.Accounts.User
     has_many :comments, Blogging.Contents.Comments.Comment
 
