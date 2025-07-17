95544ce530b37115cd2742c1ce047f047e805eec
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-17 21:23:10 +0530

    boomark module

--- lib/blogging_web/live/bookmark_live/index.ex
+++ lib/blogging_web/live/bookmark_live/index.ex
@@ -1,3 +1,50 @@
 defmodule BloggingWeb.BookmarkLive.Index do
+  use BloggingWeb, :live_view
 
+  alias Blogging.Contents.Bookmarks.Bookmarks
+  alias Blogging.Accounts
+
+  @per_page 10
+
+  def mount(_params, session, socket) do
+    current_user = Accounts.get_user_by_session_token(session["user_token"])
+
+    page = 1
+    total_count = Bookmarks.count_user_bookmarks(current_user.id)
+
+    total_pages =
+      if total_count == 0 do
+        1
+      else
+        Float.ceil(total_count / @per_page) |> trunc()
+      end
+
+    bookmarks = Bookmarks.list_user_bookmarks(current_user.id, page, @per_page)
+
+    IO.inspect(bookmarks, label: "Bookmarks in mount")
+
+    {:ok,
+     socket
+     |> assign(:user_id, current_user.id)
+      |> assign(:current_user, current_user)
+     |> assign(:bookmarks, bookmarks)
+     |> assign(:loaded_page, page)
+     |> assign(:total_pages, total_pages)}
+  end
+
+  def handle_event("load-more", _params, socket) do
+    page = socket.assigns.loaded_page + 1
+    user_id = socket.assigns.user_id
+
+    if page <= socket.assigns.total_pages do
+      new_bookmarks = Bookmarks.list_user_bookmarks(user_id, page, @per_page)
+
+      {:noreply,
+       socket
+       |> assign(:bookmarks, socket.assigns.bookmarks ++ new_bookmarks)
+       |> assign(:loaded_page, page)}
+    else
+      {:noreply, socket}
+    end
+  end
 end
