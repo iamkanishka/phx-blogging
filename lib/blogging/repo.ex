f713ab21684dcdd772280adaa4310584b569a9ca
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-23 21:34:45 +0530

    minor chnage

--- lib/blogging/repo.ex
+++ lib/blogging/repo.ex
@@ -3,6 +3,5 @@ defmodule Blogging.Repo do
     otp_app: :blogging,
     adapter: Ecto.Adapters.Postgres
 
-  @migration_primary_key [type: :binary_id]
-  @migration_foreign_key [type: :binary_id]
+
 end
