3bf25fff002a0ba12341f93db77a50b140afeebb
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-07-23 21:35:22 +0530

    chore: minor changes

--- lib/blogging/contents/comments/comment.ex
+++ lib/blogging/contents/comments/comment.ex
@@ -1,6 +1,7 @@
 defmodule Blogging.Contents.Comments.Comment do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Blogging.Contents.Comments.Comment
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @derive {Phoenix.Param, key: :id}
@@ -8,50 +9,63 @@ defmodule Blogging.Contents.Comments.Comment do
   schema "comments" do
     field :content, :string
     field :depth, :integer, default: 0
+    # Optional if you later add path-based traversal
+    # field :path, :string
 
     belongs_to :user, Blogging.Accounts.User
     belongs_to :post, Blogging.Contents.Posts.Post
-    belongs_to :parent, Blogging.Contents.Comments.Comment
+    belongs_to :parent, Comment
 
-    has_many :replies, Blogging.Contents.Comments.Comment, foreign_key: :parent_id
-    has_many :reactions, Blogging.Contents.Reactions.Reaction
+    has_many :replies, Comment, foreign_key: :parent_id
+    has_many :reactions, Blogging.Contents.Reactions.Reaction, foreign_key: :reactable_id,
+      where: [reactable_type: "comment"]
 
     timestamps()
   end
 
+  @doc false
   def changeset(comment, attrs) do
     comment
-    |> cast(attrs, [:content, :user_id, :post_id, :parent_id, :depth])
+    |> cast(attrs, [:content, :user_id, :post_id, :parent_id])
     |> validate_required([:content, :user_id, :post_id])
     |> validate_length(:content, max: 1000)
-    |> validate_depth()
-    |> set_path()
+    |> foreign_key_constraint(:user_id, name: "comments_user_id_fkey")
+    |> foreign_key_constraint(:post_id, name: "comments_post_id_fkey")
+    |> foreign_key_constraint(:parent_id, name: "comments_parent_id_fkey")
+    |> validate_and_set_depth()
+    # |> set_path()
   end
 
-  defp validate_depth(changeset) do
-    case get_field(changeset, :parent_id) do
-      nil ->
-        changeset
+  defp validate_and_set_depth(changeset) do
+    parent_id = get_change(changeset, :parent_id)
 
-      _parent_id ->
-        parent_depth = get_field(changeset, :depth) || 0
+    if is_nil(parent_id) do
+      put_change(changeset, :depth, 0)
+    else
+      case Blogging.Repo.get(Comment, parent_id) do
+        nil ->
+          add_error(changeset, :parent_id, "Parent comment not found")
 
-        if parent_depth >= 2 do
-          add_error(changeset, :parent_id, "Maximum comment depth reached")
-        else
+        %Comment{depth: parent_depth} when parent_depth >= 2 ->
+          add_error(changeset, :depth, "Maximum nesting (2 levels) reached")
+
+        %Comment{depth: parent_depth} ->
           put_change(changeset, :depth, parent_depth + 1)
-        end
+      end
     end
   end
 
-  defp set_path(changeset) do
-    case get_field(changeset, :parent_id) do
-      nil ->
-        changeset
-
-      parent_id ->
-        parent_path = get_field(changeset, :path) || ""
-        put_change(changeset, :path, "#{parent_path}.#{parent_id}")
-    end
-  end
+  # Optional ltree path field if you decide to support ancestry navigation
+  # defp set_path(changeset) do
+  #   case get_change(changeset, :parent_id) do
+  #     nil -> put_change(changeset, :path, "")
+  #     parent_id ->
+  #       case Blogging.Repo.get(Comment, parent_id) do
+  #         nil -> changeset
+  #         %Comment{path: parent_path} ->
+  #           new_path = parent_path <> "." <> to_string(parent_id)
+  #           put_change(changeset, :path, new_path)
+  #       end
+  #   end
+  # end
 end
