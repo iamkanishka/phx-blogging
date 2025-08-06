e62628ec07ac6deea68cc75fb1ba22e5284d9707
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-08-06 18:44:32 +0530

    Added path

--- priv/repo/migrations/20250707115618_create_comments.exs
+++ priv/repo/migrations/20250707115618_create_comments.exs
@@ -3,14 +3,14 @@ defmodule Blogging.Repo.Migrations.CreateComments do
 
   def change do
     # Optional: Enable `ltree` extension if using hierarchical path indexing
-    # execute("CREATE EXTENSION IF NOT EXISTS ltree")
+    execute("CREATE EXTENSION IF NOT EXISTS ltree")
 
     create table(:comments, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :content, :text, null: false
       add :depth, :integer, null: false, default: 0
       # Optional ltree support:
-      # add :path, :ltree
+      add :path, :ltree
 
       add :user_id, references(:users, on_delete: :delete_all, type: :binary_id), null: false
       add :post_id, references(:posts, on_delete: :delete_all, type: :binary_id), null: false
