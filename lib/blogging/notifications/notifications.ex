1ae89b535ed06e1fa2e227903b80dba5b611f3b4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-09-16 21:14:26 +0530

    refactor: Refactor notifications module to include type-specific helpers and message building.

--- lib/blogging/notifications/notifications.ex
+++ lib/blogging/notifications/notifications.ex
@@ -62,48 +62,124 @@ defmodule Blogging.Notifications.Notifications do
   # end
 
   defp create_notification(user_id, type, data) do
-    if type in Ecto.Enum.values(Notification, :type) do
+
+    message = build_message(type, data)
+
+  changeset =
       %Notification{}
-      |> Notification.changeset(%{user_id: user_id, type: type, data: data})
-      |> Repo.insert()
-    else
-      {:error, :invalid_type}
+      |> Notification.changeset(%{
+        user_id: user_id,
+        type: type,
+        data: data,
+        message: message
+      })
+
+      case Repo.insert(changeset) do
+      {:ok, notification} ->
+        broadcast_new_notification(notification)
+        {:ok, notification}
+
+      {:error, changeset} ->
+        {:error, changeset}
+
+      other ->
+        {:error, other}
     end
+  end
+
+  defp broadcast_new_notification(notification) do
+    BloggingWeb.Endpoint.broadcast(
+      "notifications:#{notification.user_id}",
+      "new_notification",
+      %{notification: notification}
+    )
+  end
+
+  defp build_message(:comment, data) do
+    "#{data.name} commented on your post"
+  end
+
+  defp build_message(:reply, data) do
+    "#{data.name} replied to a comment on your post"
+  end
+
+  defp build_message(:reaction, data) do
+    "#{data.name} reacted with #{data.emoji}"
+  end
+
+  defp build_message(:follow, data) do
+
+    "#{data.name} followed you"
+  end
+
+  defp build_message(:subscription, data) do
+     "#{data.name} subscribed to get email notifications for your stories"
+  end
+
+  defp build_message(:system, msg) do
+    msg
   end
 
   # -------------------------
   # Type-Specific Helpers
   # -------------------------
 
-  def notify_comment(user_id, comment_id, post_id) do
-    create_notification(user_id, :comment, %{comment_id: comment_id, post_id: post_id})
+  def notify_comment(user_id, commentor_id, post_id, commenter_name) do
+  create_notification(user_id, :comment, %{
+    notifier_id: commentor_id,
+    post_id: post_id,
+    name: commenter_name
+  })
+end
+
+def notify_reply(user_id, commentor_id, post_id, commenter_name) do
+  create_notification(user_id, :reply, %{
+    notifier_id: commentor_id,
+    post_id: post_id,
+    name: commenter_name
+  })
+end
+
+def notify_post_reaction(user_id, reactable_type, reactable_id, reactor_id, emoji, reactor_name) do
+  create_notification(user_id, :reaction, %{
+    reactable_type: reactable_type,
+    reactable_id: reactable_id,
+    emoji: emoji,
+    notifier_id: reactor_id,
+    name: reactor_name
+  })
+end
+
+def notify_follow(user_id, follower_id, follower_name) do
+  create_notification(user_id, :follow, %{
+    notifier_id: follower_id,
+    name: follower_name
+  })
+end
+
+def notify_subscription(user_id, subscriber_id, subscriber_name) do
+  create_notification(user_id, :subscription, %{
+    notifier_id: subscriber_id,
+    name: subscriber_name
+  })
+end
+
+@spec notify_system(any(), any()) :: {:error, any()} | {:ok, any()}
+def notify_system(user_id, message) do
+  create_notification(user_id, :system, %{
+    notifier_id: nil,
+    message: message
+  })
+end
+
+
+  defp format_timestamp(naive_dt) do
+    # e.g. "2 days ago"
+    Timex.format!(naive_dt, "{relative}", :relative)
   end
 
-  def notify_reaction(user_id, reactable_type, reactable_id, emoji) do
-    create_notification(user_id, :reaction, %{
-      reactable_type: reactable_type,
-      reactable_id: reactable_id,
-      emoji: emoji
-    })
-  end
-
-  def notify_mention(user_id, mentioned_by_id, post_id) do
-    create_notification(user_id, :mention, %{mentioned_by: mentioned_by_id, post_id: post_id})
-  end
-
-  def notify_follow(user_id, follower_id) do
-    create_notification(user_id, :follow, %{follower_id: follower_id})
-  end
-
-  def notify_subscription(user_id, subscriber_id) do
-    create_notification(user_id, :subscription, %{subscriber_id: subscriber_id})
-  end
-
-  def notify_clap(user_id, post_id, clapper_id) do
-    create_notification(user_id, :clap, %{post_id: post_id, clapper_id: clapper_id})
-  end
-
-  def notify_system(user_id, message) do
-    create_notification(user_id, :system, %{message: message})
+  def some_function() do
+    message = build_message(:comment, %{"name" => "Kanishka"})
+    IO.puts(message)
   end
 end
