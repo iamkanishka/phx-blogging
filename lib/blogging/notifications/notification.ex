d5bceb038269d30afd08b4f2ea07c9df54f6b0a1
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-09-16 21:14:04 +0530

    feat: Added fields to Notification schema and updated changeset validation.

--- lib/blogging/notifications/notification.ex
+++ lib/blogging/notifications/notification.ex
@@ -2,7 +2,7 @@ defmodule Blogging.Notifications.Notification do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @types [:comment, :reaction, :mention]
+  @types [:comment, :reaction, :reply,  :mention, :follow, :system, :subscription]
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @derive {Phoenix.Param, key: :id}
@@ -12,6 +12,7 @@ defmodule Blogging.Notifications.Notification do
     field :type, Ecto.Enum, values: @types
     field :data, :map
     field :read_at, :naive_datetime
+    field :message, :string
 
     belongs_to :user, Blogging.Accounts.User
 
@@ -20,7 +21,7 @@ defmodule Blogging.Notifications.Notification do
 
   def changeset(notification, attrs) do
     notification
-    |> cast(attrs, [:type, :data, :user_id, :read_at])
-    |> validate_required([:type, :data, :user_id])
+    |> cast(attrs, [:type, :data, :message,  :user_id, :read_at])
+    |> validate_required([:type, :message, :data, :user_id])
   end
 end
