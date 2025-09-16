2696dfc01c9dab1b4be87bd65067a25b4b39b968
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-09-16 21:15:51 +0530

    refactor: Refactor notification live view to use Notifications module and handle new notifications.

--- lib/blogging_web/live/notification_live/index.ex
+++ lib/blogging_web/live/notification_live/index.ex
@@ -1,95 +1,28 @@
 defmodule BloggingWeb.NotificationLive.Index do
-  alias Blogging.Accounts
   use BloggingWeb, :live_view
 
-  alias Blogging.Contents.Posts.{Posts}
+  alias Blogging.Notifications.Notifications
+  alias Blogging.Accounts
 
   @impl true
   def mount(_params, session, socket) do
     current_user = Accounts.get_user_by_session_token(session["user_token"])
 
-    activities = [
-      %{
-        name: "Felix da Maa",
-        avatar: "https://example.com/felix.png",
-        action: "clapped for 🥳😍",
-        timestamp: "2 days ago"
-      },
-      %{
-        name: "Narghiza Ergashova",
-        avatar: "https://example.com/narghiza.png",
-        action: "clapped for 🥰😃",
-        timestamp: "3 days ago"
-      },
-      %{
-        name: "Eilen Lexus",
-        avatar: "https://example.com/eilen.png",
-        action:
-          "clapped for <span class=\"font-medium\">Next.js vs Phoenix: Choosing the Right Framework for Your Project</span>",
-        timestamp: "4 days ago"
-      },
-      %{
-        name: "Kaushalsinh",
-        avatar: nil,
-        action: "subscribed to get email notifications for your stories",
-        timestamp: "Jul 12, 2025"
-      },
-      %{
-        name: "Kaushalsinh",
-        avatar: nil,
-        action: "followed you",
-        timestamp: "Jul 12, 2025"
-      },
-      %{
-        name: "CodeQuest",
-        avatar: "https://example.com/codequest.png",
-        action: "followed you",
-        timestamp: "Jul 10, 2025"
-      },
-      %{
-        name: "yuuenchi + 1 other",
-        avatar: nil,
-        action: "followed you",
-        timestamp: "Jul 5, 2025"
-      },
-      %{
-        name: "S Kamalakannan",
-        avatar: "https://example.com/s_karma.png",
-        action: "subscribed to get email notifications for your stories",
-        timestamp: "Jul 1, 2025"
-      },
-      %{
-        name: "S Kamalakannan",
-        avatar: "https://example.com/s_karma.png",
-        action: "followed you",
-        timestamp: "Jul 1, 2025"
-      },
-      %{
-        name: "Kratagya Tripathi",
-        avatar: "https://example.com/kratagya.png",
-        action: "followed you",
-        timestamp: "Jun 30, 2025"
-      },
-      %{
-        name: "Kratagya Tripathi",
-        avatar: "https://example.com/kratagya.png",
-        action: "subscribed to get email notifications for your stories",
-        timestamp: "Jun 30, 2025"
-      },
-      %{
-        name: "Elias Haider",
-        avatar: "https://example.com/elias.png",
-        action: "followed you",
-        timestamp: "Jun 10, 2025"
-      }
-    ]
+    if connected?(socket) do
+      BloggingWeb.Endpoint.subscribe("notifications:#{current_user.id}")
+    end
+
+    notifications =
+      current_user
+      |> Notifications.list_notifications()
+
+    IO.inspect(notifications, label: "Notifications")
 
     {:ok,
      socket
-     |> assign(:posts, list_posts(current_user.id))
      |> assign(:current_user_id, current_user.id)
      |> assign(:current_user, current_user)
-     |> assign(:activities, activities)
+     |> assign(:notifications, notifications)
      |> assign(:page_title, "Posts")
      |> assign(:pagination, nil)}
   end
@@ -110,14 +43,16 @@ defmodule BloggingWeb.NotificationLive.Index do
   end
 
   @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    post = Posts.get_post(id)
-    {:ok, _} = Posts.delete_post(post)
 
-    {:noreply, assign(socket, :posts, list_posts(socket.assigns.current_user_id))}
-  end
-
-  defp list_posts(user_id) do
-    Posts.list_posts_by_user(user_id, page: 1, page_size: 10)
+  def handle_info(
+        %{
+          event: "new_notification",
+          payload: %{notification: notification}
+        },
+        socket
+      ) do
+    {:noreply,
+     socket
+     |> assign(:notifications, [notification | socket.assigns.notifications])}
   end
 end
