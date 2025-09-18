f4974ce8d7dc16e4aafd572c55cc7940f335099b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-09-18 21:34:23 +0530

    feat: Added notification badge handling and updated user settings live module

--- lib/blogging_web/live/post_live/index.ex
+++ lib/blogging_web/live/post_live/index.ex
@@ -1,5 +1,5 @@
 defmodule BloggingWeb.PostLive.Index do
-alias Blogging.Accounts
+  alias Blogging.Accounts
   use BloggingWeb, :live_view
 
   alias Blogging.Contents.Posts.{Posts}
@@ -11,23 +11,22 @@ alias Blogging.Accounts
     {:ok,
      socket
      |> assign(:posts, list_posts(current_user.id))
-      |> assign(:current_user, current_user)
-
-      |> assign(:current_user_id, current_user.id)
+     |> assign(:current_user, current_user)
+     |> assign(:has_new_notifications, false)
+     |> assign(:current_user_id, current_user.id)
      |> assign(:page_title, "Posts")
      |> assign(:pagination, nil)}
   end
 
   @impl true
   def handle_params(params, url, socket) do
-     current_path = URI.parse(url).path
+    current_path = URI.parse(url).path
 
     {:noreply,
      socket
      |> assign(:current_path, current_path)
-     |>  apply_action(socket.assigns.live_action, params)}
+     |> apply_action(socket.assigns.live_action, params)}
   end
-
 
   defp apply_action(socket, :index, _params) do
     socket
@@ -43,8 +42,17 @@ alias Blogging.Accounts
   end
 
   defp list_posts(user_id) do
-   data =  Posts.list_posts_by_user(user_id,  page: 1, page_size: 10)
-  #  IO.inspect(data, label: "Posts Data")
-   data
+    data = Posts.list_posts_by_user(user_id, page: 1, page_size: 10)
+    #  IO.inspect(data, label: "Posts Data")
+    data
+  end
+
+  @impl true
+  def handle_info(
+        %{event: "render_new_notification_badge", payload: %{notification: _notification}},
+        socket
+      ) do
+    IO.inspect("Received new notification badge")
+    {:noreply, assign(socket, :has_new_notifications, true)}
   end
 end
